5b1e050570c755bbd1df586a6076b8d3ba4b0a6a
vishalnpasi <vishalnpasi@gmail.com>
2022-08-08 16:22:18 +0530

    W4D1 New Api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
+const underScore = require('underscore')
+
 //....logger file importing...
 const log = require('../logger/logger')
 
@@ -12,6 +14,8 @@ const util = require('../util/helper')
 //....validator file importing.......
 
 const validator = require('../validator/formatter')
+
+let Lodash = require('lodash');     //importing lodash package.........
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -37,12 +41,62 @@ router.get('/test-me', function (req, res) {
      validator.changeToLowerCase()
 
      validator.changeToUpperCase()
+
+    let weekend = ['Saturday','Sunday']
     
+    let result = underScore.first(weekend , 2)
+    
+    console.log('UnderScore example result is:',result)
+
+    let monthsArray=['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+
+    //splited on Array into 4 equality...
+
+    let subArray =Lodash.chunk(monthsArray,4)
+
+    console.log(subArray)
+    
+    //select 9 last element on array.......
+
+    let oddNum = [1,3,5,7,9,11,13,15,17,19]
+
+    console.log(Lodash.tail(oddNum))
+
+    //marege duplicate ....
+    console.log(Lodash.union([1,2,3,4,5],[3,4,5],[6,7,7],[8,9,10]))
+
+    //make pairs....
+
+    console.log(Lodash.fromPairs([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]))
+
 });
 
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
+})
+
+router.get('/cohort-members',function(req,res){
+    let students = ['sabiha','Neha','Akash']
+    res.send(students)
+})
+
+router.get('/students',function(req,res){
+    let details = 'Sabiha'
+    res.send(details)
+    
+})
+
+router.get('/student-details/:name/:age',function(req,res){
+    console.log('this is the request '+JSON.stringify(req.params))
+    let reqParams = req.params
+    let studentName = reqParams.name
+    let studentAge = reqParams.age
+     console.log('Name of the Student is ', studentName)
+     console.log("Age of the Student is  ",studentAge)
+     res.send(studentName+" "+studentAge)
+    // let details = ["Neha Sharma",'Sabiha Khan']
+    // res.send(details)
 })
 
 router.get('/give-me-students-data',function(req, res){
