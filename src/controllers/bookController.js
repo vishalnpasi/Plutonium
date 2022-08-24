f41ba7bf17b88a9ef36f6268abfa82f957262da9
vishalnpasi <vishalnpasi@gmail.com>
2022-08-24 15:21:35 +0530

    updated

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -30,10 +30,34 @@ const getAllBook = async function(req,res){
 }
 
 const updateBooks = async function(req,res){
-   // let AllBooks = await bookModel.find({ "publisherId.name": "Penguin"}).populate("publisherId")
-    // let AllBooks = await bookModel.updateMany({publisherId :{name :{$in :["Penguin","HarperCollins"]}},{$set :{iHardCover:true}},{new:true}).populate("publisherId")
-    let AllBooks = await bookModel.find().populate("publisherId")
-    res.send({msg:AllBooks})
+    //part a)
+    // sol 1..
+    let specificPublisher = await publisherModel.find({name:{$in:["Penguin","Harper Collins"]}}).select({_id:1})
+    let allId = specificPublisher.map(obj => obj.id)        //converting Array of Object to Only Array...
+    let result = await bookModel.updateMany({publisherId:{$in : allId}},{$set :{isHardCover:true}},{new:true})
+
+    res.send({msg:result})
+
+    // part b)
+    //find all books whose authors have rating > 3.5
+    //update the price of these books..
+    //sol 1..
+    // let specificAuthors = await authorModel.find({rating:{$gt : 3.5}}).select({_id:true})
+    // specificAuthors = specificAuthors.map(obj => obj.id)
+    // let specificBooks = await bookModel.updateMany({authorId:{$in : specificAuthors}},{$inc :{price:10}})
+    // res.send({msg:specificBooks})
+
+    // sol 2..
+    // let allBooks = await bookModel.find().populate("authorId")
+    // let specificAuthors = allBooks.filter(obj => obj.authorId.rating > 3.5)
+    // specificAuthorsId = specificAuthors.map(obj => obj.id)
+    // let specificBooks = await bookModel.updateMany({_id:{$in : specificAuthorsId}},{$inc :{price:10}})
+    //  res.send({msg:specificBooks})
+
+    //  res.send({msg:specificAuthorsId})
+    
+    // let allBooks = await bookModel.updateMany({},{price:0})
+    
 }
 module.exports.createBook= createBook
 module.exports.getAllBook = getAllBook
